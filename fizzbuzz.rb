a3a7de73a521d447041069eb81ab80aeeee74b30
Evan Delasota <evan.delasota@gmail.com>
2020-02-07 02:39:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(integer)
-  if (integer % 3 == 0)
+  if (integer % 3 == 0 && integer % 5 == 0)
     return "Fizzbuzz"
+  end
+  elsif (integer % 3 == 0)
+    return "Fizz"
+  end
+  elsif (integer % 5 == 0)
+    return "Buzz"
+  end
+  else
+    return nil
   end
 end
